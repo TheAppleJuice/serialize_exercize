61abc77cbba541b05ff10694304589ea9bbb70e8
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-02-10 10:15:52 +0100

    Serialize method

---
+++ src/main/java/se/lexicon/util/PlayerCharacter.java
@@ -0,0 +1,23 @@
+package se.lexicon.util;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+public class PlayerCharacter {
+
+    public List <PlayerCharacter> serialize (List <PlayerCharacter> source, String filePath){
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+                ObjectOutputStream out = new ObjectOutputStream(fileOutputStream)
+                ){
+            out.writeObject(source);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return source;
+    }
+
+
